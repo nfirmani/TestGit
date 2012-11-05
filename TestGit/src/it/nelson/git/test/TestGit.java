776cc146c24e1b128a2b9435bc16df7e4e643956
Tinn <Tinn@nfirmani>
2012-11-05 09:16:00 +0100

    Inizio

---
+++ TestGit/src/it/nelson/git/test/TestGit.java
@@ -0,0 +1,13 @@
+package it.nelson.git.test;
+
+public class TestGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test Git");
+	}
+
+}
